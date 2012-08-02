db40f768d48961271be22dd180910e75314f3dee
shawnsandy <shawnsandy04@gmail.com>
2012-08-01 23:00:41 -0400

    Code Cleanup

--- csf_functions.php
+++ csf_functions.php
@@ -8,7 +8,7 @@
 
 /**
  * *****************************************************************************
- * Get theme setttings
+ * Get theme setttings *********************************************************
  * *****************************************************************************
  */
 
@@ -62,36 +62,37 @@ add_action('after_setup_theme', 'cwp_theme_setup');
 
 function cwp_theme_setup() {
 
-     add_theme_support('menus');
+    add_theme_support('menus');
     register_nav_menu('primary', __('Primary', 'basejump'));
     register_nav_menu('browse', __('Browse', 'basejump'));
     register_nav_menu('category', __('Categories', 'basejump'));
     register_nav_menu('about', __('About', 'basejump'));
 
     /**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on _s, use a find and replace
-	 * to change '_s' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'basejump', get_template_directory() . '/languages' );
+     * Make theme available for translation
+     * Translations can be filed in the /languages/ directory
+     * If you're building a theme based on _s, use a find and replace
+     * to change '_s' to the name of your theme in all the template files
+     */
+    load_theme_textdomain('basejump', get_template_directory() . '/languages');
 
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
+    $locale = get_locale();
+    $locale_file = get_template_directory() . "/languages/$locale.php";
+    if (is_readable($locale_file))
+        require_once( $locale_file );
 
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
+    /**
+     * Add default posts and comments RSS feed links to head
+     */
+    add_theme_support('automatic-feed-links');
 
-
-
+    core_functions::favicon();
+    add_theme_support('post-thumbnails');
+    add_theme_support('automatic-feed-links');
+    add_image_size('icon-60', 60, 60, true);
+    add_image_size('icon-100', 100, 100, true);
+    add_image_size('icon-40', 40, 40, true);
 }
-
-
-
 
 /*
  * register scripts*************************************************************
@@ -104,37 +105,6 @@ function jump_scripts() {
      * setup some script variables
      */
     $css_path = get_template_directory_uri() . '/library/css';
-
-    $cycle_style = $css_path . '/home-cycle.css';
-    $tiptip_style = $css_path . '/tipTip.css';
-
-    /*
-     * register styles
-     */
-
-    wp_register_style('cycle', $cycle_style);
-
-    //wp_register_style('cycle', $tiptip_style);
-
-
-    wp_register_style('flex-slider', get_stylesheet_directory_uri() . '/library/css/flexslider.css');
-    wp_register_style('pictogram', cwp::locate_in_library('stylesheet.css', 'fonts/pictograms') );
-
-    $js_h5f = get_template_directory_uri() . '/library/js/h5f.min.js';
-    $js_backstretch = get_template_directory_uri() . '/library/js/jquery.backstretch.min.js';
-    $js_cycle = get_template_directory_uri() . '/library/js/jquery.cycle.all.js';
-    $js_masonry = get_template_directory_uri() . '/library/js/jquery.masonry.min.js';
-    $js_tiptip = get_template_directory_uri() . '/library/js/jquery.tipTip.minified.js';
-
-
-
-    wp_register_script('h5f', $js_h5f);
-    wp_register_script('backstretch', $js_backstretch, array('jquery'));
-    wp_register_script('cycle', $js_cycle, array('jquery'));
-    wp_register_script('masonry', $js_masonry, array('jquery'));
-    wp_register_script('tiptip', $js_tiptip, array('jquery'));
-    wp_register_script('flex-slider', get_stylesheet_directory_uri() . '/library/js/jquery.flexslider-min.js', array('jquery'));
-
 
     /**
      * bootstrap scripts
@@ -169,12 +139,7 @@ function jump_scripts() {
     }
 }
 
-core_functions::favicon();
-add_theme_support('post-thumbnails');
-add_theme_support('automatic-feed-links');
-add_image_size('icon-60', 60, 60, true);
-add_image_size('icon-100', 100, 100, true);
-add_image_size('icon-40', 40, 40, true);
+
 
 
 /*
@@ -236,19 +201,6 @@ cwp_social::contact_info();
 
 
 
-// Initialize the metabox class
-//add_action('init', 'cwp_initialize_cmb_meta_boxes', 9999);
-
-//function cwp_initialize_cmb_meta_boxes() {
-//    if (!class_exists('cmb_Meta_Box')) {
-//        if (file_exists(get_template_directory() . '/includes/Custom-Metaboxes/init.php'))
-//            require_once( get_template_directory() . '/includes/Custom-Metaboxes/init.php' );
-//        elseif (file_exists(CWP_PATH . '/includes/Custom-Metaboxes/init.php')) {
-//            require_once CWP_PATH . '/includes/Custom-Metaboxes/init.php';
-//        }
-//    }
-//}
-
 /**
  * *****************************************************************************
  * custom hooks
@@ -261,41 +213,7 @@ function cwp_mobile_head() {
 function cwp_mobile_footer() {
     do_action('cwp_mobile_footer');
 }
-        /**
-     * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
-     */
-    /**
-     * *****************************************************************************
-     */
-    /**
-     * ****************************Mobile Dection********************************************************************
-     *
-     *  Available methods are isAndroid, isAndroidtablet, isIphone, isIpad, isBlackberry, isBlackberrytablet, isPalm,
-     *  isWindowsphone, isWindows, isGeneric.
-     *
-     * **************************************************************************************************************
-     */
-//    $cwp_mobile = new Mobile_Detect();
-//
-//    $cwp_isIphone = $cwp_mobile->isIphone();
-//
-//    $cwp_isMobile = $cwp_mobile->isMobile();
-//
-//    $cwp_isIpad = $cwp_mobile->isIpad();
-//
-//    $cwp_isAndroid = $cwp_mobile->isAndroidOS();
-//
-//   // $cwp_isAndroidtablet = $cwp_mobile->isAndroidtablet();
-//
-//    $cwp_isBlackberry = $cwp_mobile->isBlackberry();
-//
-//    $cwp_isBlackBerrytablet = $cwp_mobile->isBlackberrytablet();
-//
-//    $cwp_isPalm = $cwp_mobile->isPalm();
-//
-//    //$cwp_isWindowsphone = $cwp_mobile->isWindowsphone();
-//
-//    $cwp_isGeneric = $cwp_mobile->isGenericTablet();
+
 
     /**
      * *********************************Figure - Image ****************************
@@ -309,59 +227,6 @@ function cwp_mobile_footer() {
 
     add_filter('the_content', 'cwp_img_unautop', 30);
 
-    /**
-     * *****************************************************************************
-     * Borrowed Roots Functions
-     * http://www.rootstheme.com/
-     * *****************************************************************************
-     */
-// return post entry meta information
-    function cwp_roots_entry_meta() {
-        echo '<time class="updated" datetime="' . get_the_time('c') . '" pubdate>' . sprintf(__('Posted on %s at %s.', 'roots'), get_the_time('l, F jS, Y'), get_the_time()) . '</time>';
-        echo '<p class="byline author vcard">' . __('Written by', 'roots') . ' <a href="' . get_author_posts_url(get_the_author_meta('id')) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
-    }
-
-    /**
-     * create default pages
-     */
-    if (cwp::theme_options('defaultpages')) {
-        cwp_default_pages();
-    }
-
-    function cwp_default_pages($pages = array('Home', 'About', 'Contact')) {
-        global $pagenow;
-        if (is_admin() && 'themes.php' == $pagenow && isset($_GET['activated'])) {
-
-            // on theme activation make sure there's a Home page
-            // create it if there isn't and set the Home page menu order to -1
-            // set WordPress to have the front page display the Home page as a static page
-            $default_pages = array('Home');
-            $existing_pages = get_pages();
-            $temp = array();
-
-            foreach ($existing_pages as $page) {
-                $temp[] = $page->post_title;
-            }
-
-            $pages_to_create = array_diff($default_pages, $temp);
-
-            foreach ($pages_to_create as $new_page_title) {
-
-                // create post object
-                $add_default_pages = array(
-                    'post_title' => $new_page_title,
-                    'post_content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat, orci ac laoreet cursus, dolor sem luctus lorem, eget consequat magna felis a magna. Aliquam scelerisque condimentum ante, eget facilisis tortor lobortis in. In interdum venenatis justo eget consequat. Morbi commodo rhoncus mi nec pharetra. Aliquam erat volutpat. Mauris non lorem eu dolor hendrerit dapibus. Mauris mollis nisl quis sapien posuere consectetur. Nullam in sapien at nisi ornare bibendum at ut lectus. Pellentesque ut magna mauris. Nam viverra suscipit ligula, sed accumsan enim placerat nec. Cras vitae metus vel dolor ultrices sagittis. Duis venenatis augue sed risus laoreet congue ac ac leo. Donec fermentum accumsan libero sit amet iaculis. Duis tristique dictum enim, ac fringilla risus bibendum in. Nunc ornare, quam sit amet ultricies gravida, tortor mi malesuada urna, quis commodo dui nibh in lacus. Nunc vel tortor mi. Pellentesque vel urna a arcu adipiscing imperdiet vitae sit amet neque. Integer eu lectus et nunc dictum sagittis. Curabitur commodo vulputate fringilla. Sed eleifend, arcu convallis adipiscing congue, dui turpis commodo magna, et vehicula sapien turpis sit amet nisi.',
-                    'post_status' => 'publish',
-                    'post_type' => 'page'
-                );
-
-                // insert the post into the database
-                $result = wp_insert_post($add_default_pages);
-            }
-
-            //setup the default page templaets
-        }
-    }
 
     /**
      * Sets default permalink
@@ -456,6 +321,7 @@ function _s_comment( $comment, $args, $depth ) {
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
 			<footer>
 				<div class="comment-author vcard">
+
 					<?php echo get_avatar( $comment, 40 ); ?>
 					<?php printf( __( '%s <span class="says">says:</span>', 'basejump' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
 				</div><!-- .comment-author .vcard -->
@@ -489,11 +355,11 @@ function _s_comment( $comment, $args, $depth ) {
 endif; // ends check for _s_comment()
 
 
-function csf_default_menus(){
-        /**
+function csf_default_menus() {
+    /**
      * This theme uses wp_nav_menu() in one location.
+     * used in theme functions
      */
-
     // This theme styles the visual editor with editor-style.css to match the theme style.
 
 
@@ -502,41 +368,5 @@ function csf_default_menus(){
     register_nav_menu('browse', __('Browse', 'basejump'));
     register_nav_menu('category', __('Categories', 'basejump'));
     register_nav_menu('about', __('About', 'basejump'));
-
-
-
-    /**
-     * Setup default menus
-     * @based roots-theme
-     */
-
-    if (!is_nav_menu('primary')):
-        if($primary_nav = wp_get_nav_menu_object('Primary')):
-          $primary_nav_term_id = $primary_nav->term_id;
-        else :
-            $primary_nav_term_id = wp_create_nav_menu( 'Primary' );
-        endif;
-
-        $menu_items = wp_get_nav_menu_items($primary_nav_term_id);
-        if (!$menu_items OR empty($menu_items)) {
-            $pages = get_pages(array('sort_order' => 'ASC', 'number' => 5, 'child_of' => -1));
-            foreach ($pages as $page) {
-                $item = array(
-                    'menu-item-object-id' => $page->ID,
-                    'menu-item-object' => 'page',
-                    'menu-item-type' => 'post_type',
-                    'menu-item-status' => 'publish'
-                );
-                wp_update_nav_menu_item($primary_nav_term_id, 0, $item);
-            }
-            $home_menu = array(
-                'menu-item-type' => 'custom',
-                'menu-item-title' => 'Home',
-                'menu-item-attr-title' => 'Home');
-
-            wp_update_nav_menu_item($primary_nav_term_id, 0, $home_menu);
-        }
-
-    endif;
-
+    
 }
